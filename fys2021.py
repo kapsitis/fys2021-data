76193c9d122055954616aaa64edcbe98d9bec784
kapsitis <kalvis.apsitis@gmail.com>
2021-10-19 00:56:21 +0300

    Web accessible

--- fys2021.py
+++ fys2021.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, send_from_directory
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/docs')
 
 @app.route('/<path:path>')
 def send_docs(path):
